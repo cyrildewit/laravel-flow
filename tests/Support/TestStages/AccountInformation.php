1bd2e39b865964adbb9465931a0020b26935f465
Cyril de Wit <cyrilwit@gmail.com>
2019-05-21 14:35:17 +0200

    chore: add stage stubs

---
+++ tests/Support/TestStages/AccountInformation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CyrildeWit\LaravelFlow\Tests\Support\TestStages;
+
+use CyrildeWit\LaravelFlow\Stage\AbstractStage;
+
+class AccountInformation extends AbstractStage
+{
+    /**
+     * Display the account information stage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function display() {
+        //
+    }
+
+    /**
+     *
+     */
+    public function process()
+    {
+        //
+    }
+}

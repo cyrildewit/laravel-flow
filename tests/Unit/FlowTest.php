2bca432165f5dfa41636ed7442ebdba832aac010
Cyril de Wit <cyrilwit@gmail.com>
2019-05-22 17:51:36 +0200

    chore: move FlowTest to unit tests namespace

--- tests/Unit/FlowTest.php
+++ tests/Unit/FlowTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace CyrildeWit\LaravelFlow\Tests\Feature;
+namespace CyrildeWit\LaravelFlow\Tests\Unit;
 
 use CyrildeWit\LaravelFlow\Flow;
 use CyrildeWit\LaravelFlow\Tests\TestCase;
